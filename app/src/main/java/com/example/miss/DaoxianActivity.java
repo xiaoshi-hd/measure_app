5226cce73ee99a5e609b2dc1418a99358f55162c
xiaoshi-hd <1209001368@qq.com>
2019-03-11 13:27:00 +0800

    修改1

---
+++ app/src/main/java/com/example/miss/DaoxianActivity.java
@@ -0,0 +1,13 @@
+package com.example.miss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaoxianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daoxian);
+    }
+}
